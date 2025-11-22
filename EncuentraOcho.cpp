dfb13c9ed8adb501e06f3991f317bed9a4e679c2
Axelzei <viaa091201hgtylxa9@cbtis217.edu.mx>
2025-11-21 19:30:22 -0600

    agrege comentarios espesificos de cada seccion del codigo

--- EncuentraOcho.cpp
+++ EncuentraOcho.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 int main()
 {
+    // Declaro las variables que voy a usar
+    // y le doy un valor al areglo y hasta donde
+    // llega los ciclos
     int num, ban;
     cin>>num;
     int a[num];
+
+    // Le doy los varores al areglo para despues comprobar
     for(int i=0;i<num; i++){
         cin>>a[i];
     }
+
+    // Se comprueba los valores del areglo para encontrar
+    // el numero 8 o si no hay
     for(int i=0; i<num; i++){
         if(a[i]==8){
             ban=1;
@@ -16,6 +24,9 @@ int main()
             ban=0;
         }
     }
+
+    // se imprime 1 si se encontro 8
+    // se imprime 0 si no se encontro 8
     cout<<ban<<endl;
     return 0;
 }
